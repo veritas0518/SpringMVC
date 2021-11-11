6f0ac1666b519c3b9b5eaabb8b4cbd671edeb96a
xtx_global <1450985307_global@qq.com>
2021-11-11 08:54:17 +0800

    尝试自己提交代码

---
+++ springMVC-annotation/src/main/java/com/spring/mvc/config/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.spring.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName: springConfig
+ * @Description:
+ * @Author: TianXing.Xue
+ * @Date: 2021/10/9 16:36
+ **/
+
+@Configuration
+public class SpringConfig {
+}
---
+++ springMVC-annotation/src/main/java/com/spring/mvc/config/WebConfig.java
@@ -0,0 +1,112 @@
+package com.spring.mvc.config;
+
+import com.spring.mvc.interceptor.TestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @ClassName: WebConfig
+ * @Description: 代替springMVC的配置文件
+ * @Author: TianXing.Xue
+ * @Date: 2021/10/9 16:37
+ * <p>
+ * 1.扫描组件
+ * 2.视图解析器
+ * 3.view-controller
+ * 4.default-servlet-handler
+ * 5.mvc注解驱动
+ * 6.文件上传解析器
+ * 7.异常处理
+ * 8.拦截器
+ **/
+
+@Configuration //将当前类标识为一个配置类
+@ComponentScan("com.spring.mvc.controller") //1.扫描组件
+@EnableWebMvc //5.mvc注解驱动
+public class WebConfig implements WebMvcConfigurer {
+
+    //3.view-controller
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hello").setViewName("hello");
+    }
+
+    //4.default-servlet-handler
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    //6.文件上传解析器
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        return commonsMultipartResolver;
+    }
+
+    //7.异常处理
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
+        Properties properties = new Properties();
+        properties.setProperty("java.lang.ArithmeticException", "error");
+        exceptionResolver.setExceptionMappings(properties);
+        exceptionResolver.setExceptionAttribute("exception");
+        resolvers.add(exceptionResolver);
+    }
+
+    //8.拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        TestInterceptor testInterceptor = new TestInterceptor();
+        registry.addInterceptor(testInterceptor).addPathPatterns("/**");
+    }
+
+    //配置生成模板解析器
+    @Bean
+    public ITemplateResolver templateResolver() {
+        WebApplicationContext webApplicationContext = ContextLoader.getCurrentWebApplicationContext();
+        ServletContextTemplateResolver templateResolver =
+                new ServletContextTemplateResolver(webApplicationContext.getServletContext());
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    //生成模板引擎为模板引擎注入模板解析器
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    //生成视图解析器并为解析器注入模板引擎
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+
+}
